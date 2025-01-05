f3a776a80781bb88c13e607492ea7e7c9a29787c
Mārtiņš Liberts <martins.liberts@gmail.com>
2025-01-06 00:39:14 +0200

    Update data processing script and .gitignore for improved file handling and output

--- code/50-compare.R
+++ code/50-compare.R
@@ -1,18 +1,26 @@
-# R10 compare results
+# Compare results
 
 rm(list = ls())
 gc()
 
+# options
+options("openxlsx2.numFmt" = "#.000")
+
 # Results by Peter
 
-fn <- max(list.files(path = "reports",
-                     pattern = "ESS10 sample parameter estimates v1[0-9]",
-                     full.names = T))
+fn <- sort(list.files(
+  path = "reports",
+  pattern = "^ESS[0-9]{2} sample parameter estimates v[0-9]+",
+  full.names = TRUE
+))
+
 dat_p <- openxlsx2::read_xlsx(
-  xlsxFile = fn,
-  skipEmptyRows = TRUE,
-  startRow = 4,
-  check.names = TRUE
+  file = max(fn),
+  start_row = 3,
+  skip_empty_rows = TRUE,
+  skip_empty_cols = TRUE,
+  na.strings = c("NA", "#DIV/0!"),
+  check_names = TRUE
 )
 
 setDT(dat_p, check.names = TRUE)
@@ -49,77 +57,94 @@ names(dat_p)
 
 # Results by Martin
 
-fn <- max(list.files(path = "results", pattern = "xlsx$",
-                     full.names = T, recursive = T))
+fn <- sort(list.files(
+  path = "results",
+  pattern = "xlsx$",
+  full.names = TRUE,
+  recursive = TRUE
+))
+
 dat_m_c <- openxlsx2::read_xlsx(
-  xlsxFile = fn,
+  file = max(fn),
   sheet = "cntry",
-  check.names = TRUE
+  check_names = TRUE
 )
 setDT(dat_m_c)
 
 dat_m_d <- openxlsx2::read_xlsx(
-  xlsxFile = fn,
+  file = max(fn),
   sheet = "domain",
-  check.names = TRUE
+  check_names = TRUE
 )
 setDT(dat_m_d)
 
-dat_m_c <- dat_m_c[essround == "R10"]
-dat_m_d <- dat_m_d[essround == "R10"]
+latest_ess_round <- dat_m_c[, max(essround)]
+
+dat_m_c <- dat_m_c[essround == latest_ess_round]
+dat_m_d <- dat_m_d[essround == latest_ess_round]
 
 dat_m_c[, domain := "D0"]
 dat_m_d[, .N, keyby = .(domain)]
 
-dat_m <- rbindlist(list(dat_m_c, dat_m_d), use.names = T, fill = T)
+dat_m <- rbindlist(list(dat_m_c, dat_m_d), use.names = TRUE, fill = TRUE)
 rm(dat_m_c, dat_m_d)
 
 names(dat_p)
 names(dat_m)
 
-setnames(x = dat_p,
-         old = c("neff"),
-         new = c("n_eff"))
+setnames(
+  x = dat_p,
+  old = c("neff"),
+  new = c("n_eff")
+)
 
-setnames(x = dat_m,
-         old = c("deff_p", "deff_c", "ICC", "b"),
-         new = c("deffp", "deffc", "roh", "b_bar"))
+setnames(
+  x = dat_m,
+  old = c("deff_p", "deff_c", "ICC", "b"),
+  new = c("deffp",  "deffc",  "roh", "b_bar")
+)
 
-dat_m <- dat_m[, names(dat_p), with = F]
+dat_m <- dat_m[, names(dat_p), with = FALSE]
 
-dat_p_long <- melt.data.table(data = dat_p, id.vars = c("cntry", "domain"),
-                              na.rm = TRUE) |> unique()
+dat_p_long <- melt.data.table(
+  data = dat_p,
+  id.vars = c("cntry", "domain"),
+  na.rm = TRUE
+) |> unique()
 
 anyDuplicated(dat_p_long, by = c("cntry", "domain", "variable"))
 dat_p_long[, n := .N, by = c("cntry", "domain", "variable")]
 dat_p_long[n > 1]
 dat_p_long[, n := NULL]
 
-dat_m_long <- melt.data.table(data = dat_m, id.vars = c("cntry", "domain"),
-                              na.rm = TRUE) |> unique()
+dat_m_long <- melt.data.table(
+  data = dat_m,
+  id.vars = c("cntry", "domain"),
+  na.rm = TRUE
+) |> unique()
 
 anyDuplicated(dat_m_long, by = c("cntry", "domain", "variable"))
 
-dat_long <- merge(dat_p_long, dat_m_long,
-                  by = c("cntry", "domain", "variable"),
-                  all = TRUE,
-                  suffixes = c("_P", "_M"))
-
-dat_long[, diff := value_M - value_P]
-
-dat_long
-
-dat_wide <- dcast.data.table(data = dat_long,
-                             formula = cntry + domain ~ variable,
-                             value.var = c("value_P", "value_M", "diff"))
-
-write_xlsx(
-  x = dat_wide,
-  file = glue::glue("tables/compare-ess10-sample-par-est-{Sys.Date()}.xlsx"),
-  asTable = F,
-  na.strings = ""
+dat_long <- merge(
+  x = dat_p_long,
+  y = dat_m_long,
+  by = c("cntry", "domain", "variable"),
+  all = TRUE,
+  suffixes = c("_P", "_M")
 )
 
+dat_long[, diff := value_M - value_P]
+dat_long
+
+dat_wide <- dcast.data.table(
+  data = dat_long,
+  formula = cntry + domain ~ variable,
+  value.var = c("value_P", "value_M", "diff")
+)
+
+x <- grep("n_(gross|net)", names(dat_wide), value = TRUE)
+dat_wide[, c(x) := map(.SD, as.integer), .SDcols = x]
+rm(x)
 
 # Gross sample size
 dat_long[variable == "n_gross" & abs(diff) > 0][order(abs(diff))]
@@ -128,26 +153,50 @@ dat_long[variable == "n_gross" & abs(diff) > 0][order(abs(diff))]
 dat_long[variable == "n_net" & abs(diff) > 0][order(abs(diff))]
 
 # Gross sample size
-dat_wide[
+tab_n_gross <- dat_wide[
   abs(diff_n_gross) > 0,
   .(cntry, domain, value_P_n_gross, value_M_n_gross, diff_n_gross)
-][order(diff_n_gross)]
+]
+tab_n_gross
 
 # Net sample size
-dat_wide[
+tab_n_net <- dat_wide[
   abs(diff_n_net) > 0,
   .(cntry, domain, value_P_n_net, value_M_n_net, diff_n_net)
-][order(diff_n_net)]
+]
+tab_n_net
 
 # Other parameters
-dat_x <- dat_wide[abs(diff_n_net) == 0, .(cntry, domain)]
+dat_y <- dat_long[abs(diff) > 0.001]
+x <- dat_y[, .SD, .SDcols = is.numeric] |> names()
+dat_y[, c(x) := map(.SD, round, 3), .SDcols = x]
+rm(x)
 
-dat_y <- dat_long[dat_x][abs(diff) > 0.001]
+tab_ri <- dat_y[grep("ri", variable)]
+tab_rr <- dat_y[grep("rr", variable)]
+tab_deffp <- dat_y[grep("deffp", variable)]
+tab_roh <- dat_y[grep("roh", variable)]
+tab_b_bar <- dat_y[grep("b_bar", variable)]
 
-dat_y[grep("ri|rr", variable)]
 
-dat_y[grep("deffp", variable)][order(diff)]
-dat_y[grep("roh", variable)][order(diff)]
-dat_y[grep("b_bar", variable)][order(diff)]
+# Save results
 
-dat_long[cntry == "HU"]
+openxlsx2::write_xlsx(
+  x = list(
+    all_wide = dat_wide,
+    all_long = dat_long,
+    n_gross = tab_n_gross,
+    n_net = tab_n_net,
+    ri = tab_ri,
+    rr = tab_rr,
+    deffp = tab_deffp,
+    roh = tab_roh,
+    b_bar = tab_b_bar
+  ),
+  file = glue::glue(
+    "tables/compare-ESS-{latest_ess_round}-sample-par-est-{Sys.Date()}.xlsx"
+  ),
+  na.strings = "",
+  first_row = TRUE,
+  first_col = TRUE,
+)
